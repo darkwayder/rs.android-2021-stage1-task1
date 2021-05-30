eb2589a219d537fee1333ee35e0e105c8e8c67b0
darkwayder <dumpz63@gmail.com>
2021-05-30 19:26:06 +0400

    refactor: add solution

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,10 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        var needAnnaSum=(bill.sum()-bill[k])/2
+        if (needAnnaSum==b)
+            return "Bon Appetit"
+         else
+            return (b-needAnnaSum).toString()
     }
 }
